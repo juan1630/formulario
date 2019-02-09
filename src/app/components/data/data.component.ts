fdc57f67dd339dbcdf0e7859ce3e7e0053fe574c
jose <josejuanpatron1630@gmail.com>
2019-02-08 23:26:52 -0600

    Finalizacion de la seccion

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-data',
@@ -12,11 +13,12 @@ export class DataComponent  {
   usuario:any={
   
     nombreCompleto:{
-      nombre:"Juan",
-      apellido:"Patrón"
+      nombre:"",
+      apellido:""
     }, 
 
-    correo:"juan@gmail.com"
+    correo:"juan@gmail.com",
+    pasatiempos:["Correr"]
   }
 
   constructor() { 
@@ -24,20 +26,39 @@ export class DataComponent  {
 
     this.forma = new FormGroup({
       'nombreCompleto': new FormGroup({
-        'nombre':new FormControl(this.usuario.nombreCompleto.nombre, [Validators.required,
-                                      Validators.minLength(3)]),
-        'apellido':new FormControl(this.usuario.nombreCompleto.apellido, [Validators.required, 
-                                        Validators.minLength(3)])
+        'nombre':new FormControl('', [Validators.required,
+                                     Validators.minLength(3)]),
+        'apellido':new FormControl('', [Validators.required, 
+                                        this.noHerrera])
         }),       
         
-        'correo':new FormControl(this.usuario.correo , [Validators.required,
-                                  Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
+        'correo':new FormControl('' , [Validators.required,
+                                  Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
+          'pasatiempos': new FormArray([ new FormControl('',  Validators.required)]),
+          'username': new FormControl('', Validators.required, this.existeUsuario),
+          'password1': new FormControl('', Validators.required),
+          'password2': new FormControl( '', Validators.required) 
   
-  
-
+          //validators de tipo pattern
+ 
     })
    
-this.forma.setValue( this.usuario );
+//this.forma.setValue( this.usuario );
+//debe tener la misma estrucutra que el objeto
+
+this.forma.controls['password2'].setValidators([
+  Validators.required, this.noIgual.bind( this.forma )
+])
+
+this.forma.controls['username'].valueChanges
+          .subscribe( (data)=>{
+            console.log(data)
+          } )
+
+          this.forma.controls['username'].statusChanges
+          .subscribe( (data)=>{
+            console.log(data)
+          } )
 
 }
 
@@ -47,9 +68,64 @@ this.forma.setValue( this.usuario );
     console.log(this.forma)
   }
 
+  agergarPasatiempo(){
+(<FormArray> this.forma.controls['pasatiempos']).push(
+  new FormControl("Dormir", Validators.required)
+)
+
+  }
+
+  noHerrera( control:FormControl ){
+      if(control.value=="herrera"){
+        return {
+          noHerrera:true
+        }
+      }
+      return null;
+  }
+
+  noIgual( control:FormControl ){
+    //console.log(this);
+
+    let forma:any = this;
+
+    if(control.value !== forma.controls['password1'].value){
+      return {
+        noIgual:true
+      }
+    }
+    return null;
+  }
+
+
+  existeUsuario( control: FormControl ): Promise <any>|Observable<any>{
+
+    let promesa = new Promise(
+      (resolve, reject)=>{ 
+        setTimeout(()=> { 
+            if(control.value == "Juancho" ){
+              resolve({ existe:true });
+            }else{
+              resolve(null)
+            }
+         }, 3000)
+       }
+    )
+        return promesa;
+  }
+
 
   guardarCambios(){
     console.log(this.usuario);
+   /* this.forma.reset({
+      nombreCompleto:{
+        nombre:"",
+        apellido:""
+      },correo:""
+    } )
+ */
+    // restablece el ng pisting
+
   }
 
 }
